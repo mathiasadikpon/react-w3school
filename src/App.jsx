3581a6faef8d5ec317e6c3c59ed2e06435c25271
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-25 14:02:26 -0500

    feat: refactor App component and implement AnimeList for displaying anime data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Suspense, lazy } from "react";
+import { animeResource } from "./api/animeResource";
 
-import axios from "axios";
-
-const getAnimePage = async (page = 1) => {
-  const res = await axios.get("https://api.jikan.moe/v4/anime", {
-    params: {
-      page,
-      limit: 25, // max allowed
-    },
-  });
-
-  return res.data; // { data, pagination }
-};
-
-// Example usage
-getAnimePage(1).then((result) => {
-  console.log("Anime list:", result.data);
-  console.log("Has next page:", result.pagination.has_next_page);
-});
+const AnimeList = lazy(() => import("./components/AnimeList"));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <h1>Here is a sample</h1>
-      <h1>Here is my luck </h1>
-      <h1>Here is my luck </h1>
-      <h1>Here is my luck </h1>
-    </div>
+    <Suspense fallback={<h2>Loading anime...</h2>}>
+      <AnimeList resource={animeResource} />
+    </Suspense>
   );
 }
 
