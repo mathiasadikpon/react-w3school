845ff72cfafbbbdcbb09cfe6e8b01d4fc2a6ed81
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-25 15:12:52 -0500

    feat: refactor App component to remove Router wrapper and add Navbar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+// Lazy load pages
+const Home = lazy(() => import("./pages/Home.jsx"));
+const AnimeList = lazy(() => import("./pages/AnimeList.jsx"));
+const AnimeDetails = lazy(() => import("./pages/AnimeDetails.jsx"));
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Future routes:
+    <div className="App">
+      <Navbar />
+
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/anime" element={<AnimeList />} />
           <Route path="/anime/:id" element={<AnimeDetails />} />
-        */}
-      </Routes>
-    </Router>
+        </Routes>
+      </Suspense>
+
+      <Footer />
+    </div>
   );
 }
 
