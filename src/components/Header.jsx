dc5f025b3d3f3a9bb97350242b01c79c757cd378
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-26 07:36:34 -0500

    feat: update header title to 'Anime World 2'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar.jsx";
 export default function Header() {
   return (
     <header>
-      <h1 style={{ textAlign: "center", padding: "1rem" }}>Anime World</h1>
+      <h1 style={{ textAlign: "center", padding: "1rem" }}>Anime World 2</h1>
       <Navbar />
     </header>
   );
